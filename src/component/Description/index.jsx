f0ffb0434c185224f10abc66f371f5eb297f61be
mominahmed23 <mominahmad324@gmail.com>
2021-08-21 17:44:20 +0500

    routes done

--- src/component/Description/index.jsx
+++ src/component/Description/index.jsx
@@ -1,9 +1,13 @@
 import TextArea from "antd/lib/input/TextArea";
 import React from "react";
+import { ArrowLeftOutlined } from "@ant-design/icons";
 
 const Description = () => {
   return (
     <>
+      <div className="mb-5">
+        <ArrowLeftOutlined />
+      </div>
       <h2>Description</h2>
       <TextArea
         showCount
