3e2b260f64669d84a119abf489b57d82b3fb2452
mominahmed23 <mominahmad324@gmail.com>
2021-08-21 15:50:56 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div style={{ display: "flex" }}>
+      <div
+        className="sider"
+        style={{
+          width: 275,
+          flexShrink: 0,
+          height: "100vh",
+          backgroundColor: "aqua",
+        }}
+      >
+        <Switch>
+          <Route path="/users">
+            <div>Desc</div>
+          </Route>
+          <Route exact path="/">
+            <div>hello</div>
+          </Route>
+        </Switch>
+      </div>
+      <div
+        className="preview"
+        style={{ flexGrow: 1, backgroundColor: "grey", height: "100vh" }}
+      >
+        <h1 style={{ color: "red" }}>Preview</h1>
+      </div>
     </div>
+  </BrowserRouter>
   );
 }
 
